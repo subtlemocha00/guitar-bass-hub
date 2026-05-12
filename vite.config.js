fcc7fad3e95d8c60e814b793488cf0eca6ef59d6
Kev <kturley@gmail.com>
2026-05-12 12:34:05 -0400

    fixed deploy base for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Automatically uses correct base depending on where it's deployed
+const base = process.env.VERCEL ? '/' : '/guitar-bass-hub/'
+
 export default defineConfig({
-  base: '/guitar-bass-hub/',
+  base,
   plugins: [react()],
 })
